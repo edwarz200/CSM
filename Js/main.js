69cea4d90edd648bc3bd8483cf2bf8dd50f0de25
edwarz200 <edwarz200@hotmail.com>
2019-06-11 17:39:05 -0500

    Four i think

--- Js/main.js
+++ Js/main.js
@@ -1,10 +1,18 @@
 navigator.mediaDevices.getUserMedia({
-    video: true
-}).then(function(stream) {
-    console.log(stream)
-
-    let video = document.getElementById('video');
-    video.srcObject = stream;
-}).catch(function(error) {
+    video: true,
+    audio: false,
+}).then(
+    function(stream) {
+        var player = document.getElementById('player');
+        player.srcObject = stream;
+        player.classList.remove('oculto')
+        console.log(stream)
+        let video = document.getElementById('video');
+        video.srcObject = stream;
+        video.classList.remove('oculto');
+        var load = document.getElementById('load');
+        load.classList.add('oculto');
+    },
+).catch(function(error) {
     console.log(error)
 });
